4d686f44f6228a176d71342a17a246ba32405d30
James Clement <jameseclement@gmail.com>
2019-01-11 19:28:12 +0000

    Automatically backed up by Learn

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,14 +1,14 @@
-require "spec_helper"
+# require "spec_helper"
 
-describe "User" do 
-  let!(:user) { User.new }
-  it "has a first name" do 
-    user.first_name = "Tracy"
-    expect(user.first_name).to eq("Tracy")
-  end
+# describe "User" do 
+#   let!(:user) { User.new }
+#   it "has a first name" do 
+#     user.first_name = "Tracy"
+#     expect(user.first_name).to eq("Tracy")
+#   end
 
-  it "has a last name" do 
-    user.last_name = "Morgan"
-    expect(user.last_name).to eq("Morgan")
-  end
-end
+#   it "has a last name" do 
+#     user.last_name = "Morgan"
+#     expect(user.last_name).to eq("Morgan")
+#   end
+# end
